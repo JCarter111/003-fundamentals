beeef047f2ebe554f494a13653f8109b0958e9e7
Jackie Carter <jacqueline.carter@hotmail.co.uk>
2019-03-23 09:23:09 +0000

    Changes from Day 5 of Week1

--- app/library.js
+++ app/library.js
@@ -5,8 +5,18 @@ class Library {
 
   add(newBook) {
     this.books.push(newBook);
-
   }
-}
 
+  remove(oldBook) {
+    //keep all books except book that has ISBN that we want to remove
+    const keptBooks = books.filter(function(oldBook) {
+      if (this.books.ISBN === oldBook.ISBN) {
+        return false;
+      } else {
+        return true;
+      }
+    });    
+   //update books
+   this.books = remove(oldBook); 
+  }
 module.exports = Library;
--- test/library.test.js
+++ test/library.test.js
@@ -29,3 +29,15 @@ test("Check that a book can be added to the library", () => {
   library.add(book4);
   expect(library.books).toEqual([book1, book2, book3, book4]);
 });
+test("Check that a book can be removed from the library", () => {
+
+  const book1 = new Book ("Fantastic Mr Fox", "Roald Dahl", 789);
+  const book2 = new Book("Site Reliability Engineering", "Richard Murphy", 456);
+  const book3 = new Book("Don't Make Me Think","Steve Krug",123);
+  const book4 = new Book("Growth Mindset","Carol Dweck",992);
+
+  const library = new Library([book1, book2, book3,book4]);
+  // library.books.add()
+  library.remove(book2);
+  expect(library.books).toEqual([book1, book3, book4]);
+});
